53fe94b888641f4685d45a5aaa259da23ea045c7
mozolevsky <alf.rulit@gmail.com>
2018-01-31 16:43:03 +0300

    Redeveloped mobile menu: fixed error of cross-browser compatibility

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -33,3 +33,53 @@ window.addEventListener("scroll", function() {
     stickyHeader.classList.remove('header-sticky_opened');
   }
 });
+
+// mobile menu
+var slideout = new Slideout({
+  'panel': document.getElementById('panel'),
+  'menu': document.getElementById('menu'),
+  'padding': 256,
+  'tolerance': 70
+});
+
+// Toggle button
+  document.querySelectorAll('.js-slideout-toggle').forEach(function(item) {
+      item.addEventListener('click', function() {
+        slideout.toggle();
+    });
+  });
+
+  var fixed = document.querySelector('.header-sticky');
+
+  slideout.on('translate', function(translated) {
+      fixed.style.transform = 'translateX(' + translated + 'px)';
+  });
+
+  slideout.on('beforeopen', function () {
+      fixed.style.transition = 'transform 300ms ease';
+      fixed.style.transform = 'translateX(256px)';
+      fixed.classList.add("header-sticky_with-overlay");
+  });
+
+  slideout.on('beforeclose', function () {
+      fixed.style.transition = 'transform 300ms ease';
+      fixed.style.transform = 'translateX(0px)';
+      fixed.classList.remove("header-sticky_with-overlay");
+  });
+
+  function close(eve) {
+    eve.preventDefault();
+    slideout.close();
+  }
+  
+  slideout
+    .on('beforeopen', function() {
+      this.panel.classList.add('panel-open');
+    })
+    .on('open', function() {
+      this.panel.addEventListener('click', close);
+    })
+    .on('beforeclose', function() {
+      this.panel.classList.remove('panel-open');
+      this.panel.removeEventListener('click', close);
+});
